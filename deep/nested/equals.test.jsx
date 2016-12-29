5e5a6196b1afc59090dec0aebec0e35ff683c4a6
Sunny Hirai <thesunny@gmail.com>
2016-12-28 16:04:11 -0800

    initial commit

---
+++ deep/nested/equals.test.jsx
@@ -0,0 +1,17 @@
+import assert from 'assert'
+import equals from './equals'
+
+export default function () {
+  it('should equal if same string', function () {
+    assert.equal(true, equals('a', 'a'))
+  })
+
+  it('should not equal if different string', function () {
+    assert.equal(false, equals('a', 'b'))
+  })
+
+  it('should equal if same number', function () {
+    assert.equal(true, equals(1, 1))
+  })
+
+}
---
+++ test/test.jsx
@@ -0,0 +1,16 @@
+import {relative} from 'path'
+import glob from 'glob'
+import _ from 'underscore'
+
+// set global underscore library to simulate Meteor
+global._ = _
+
+// get all files that look like *.test.js or *.test.jsx
+const paths = glob.sync(__dirname + '/../**/*.test.@(js|jsx)')
+
+// describe path and run a test for it
+paths.forEach((path) => {
+  const relativePath = relative(__dirname + '/..', path)
+  const fn = require(path).default
+  describe(relativePath, fn)
+})
